70a0071064865e2408fec55881bb3db9a8df33b7
tejuypatil <tejuypatil@gmail.com>
2022-07-26 20:32:44 +0530

    Added program to print maximum using java generic

--- src/com/bridglab/generic/TestMaximumNumber.java
+++ src/com/bridglab/generic/TestMaximumNumber.java
@@ -4,37 +4,29 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class TestMaximumNumber <s1>{
+public class TestMaximumNumber <S>{
 
-    public s1 [] arrayInput;
+    private S[] inputArray;
 
-    //get arrayInput
-    public s1[] getArrayInput()
+    public void MaxValueUsingGenerics(S[] inputArray)
     {
-        return arrayInput;
+        this.inputArray = inputArray;
     }
 
-    //Set arrayInput
-    public void setArrayInput(s1[] arrayInput)
+    public static <T> T findMax(T[] myNumber)
     {
-        this.arrayInput = arrayInput;
-    }
-    //Constructor
-    public TestMaximumNumber(s1[] arrayInput)
-    {
-        this.arrayInput = arrayInput;
+        List myNumberList = Arrays.asList(myNumber);
+        T max = (T) Collections.max( myNumberList );
+        toPrint(max);
+        return max;
     }
 
-    public static <s1> Comparable findMax(s1[] arr)
+    private static <S> void toPrint(S a)
     {
-        List arrList = Arrays.asList(arr);
-        Comparable maxValue = Collections.max(arrList);
-
-        return maxValue;
+        System.out.println(a);
     }
-    // --------------------- MAin Method -----------------------------------
     public static void main(String[] args)
     {
-
+        System.out.println(" ************ Java Generics ********** ");
     }
 }
